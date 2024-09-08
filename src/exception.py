9ba7ecad4cd07fabaca359434d2e6afbc593b21a
sourav <souravmaity16@gmail.com>
2024-09-09 01:27:51 +0530

    logger_file

--- src/exception.py
+++ src/exception.py
@@ -5,4 +5,3 @@ def error_message_detail(error,error_detail:sys):
     error_message = "Error occured in python script name [{0}] line number  [{1}] error message[{2}]".format()
     file_name = exc_tb.tb_frame.f_code.co_filename
     file_name, exc_tb.tb_lineno, str(error)
-    
